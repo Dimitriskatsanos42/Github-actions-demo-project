09539d72f10acc6200ab77a0ae3a6a08bc8da5dc
Dimitris katsanos <dimitriskats42@gmail.com>
2026-06-23 20:23:49 +0300

    Add function to sum two numbers

---
+++ calculator.py
@@ -0,0 +1,9 @@
+def add_numbers(a, b):
+    """Adds two numbers and returns the result."""
+    return a + b
+
+
+if __name__ == "__main__":
+    result = add_numbers(5, 10)
+    print(f"The result is: {result}")
+  
