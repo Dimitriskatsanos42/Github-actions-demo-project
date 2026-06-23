5a0c82dc7bf92c08de2ffdcb7462035ffd361f62
Dimitris katsanos <dimitriskats42@gmail.com>
2026-06-23 20:28:11 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,4 +1,4 @@
-def add_numbers(a, b):
+  def add_numbers(a, b):
     """Adds two numbers and returns the result."""
     return a + b
 
@@ -6,4 +6,3 @@ def add_numbers(a, b):
 if __name__ == "__main__":
     result = add_numbers(5, 10)
     print(f"The result is: {result}")
-  
