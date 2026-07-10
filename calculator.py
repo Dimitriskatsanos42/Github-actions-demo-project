dcfcc070c4e6f7ce61e88fc862c391887e5ea989
Dimitris katsanos <dimitriskats42@gmail.com>
2026-07-10 20:23:02 +0300

    Delete calculator.py

--- calculator.py
+++
@@ -1,7 +0,0 @@
-  def add_numbers(a, b):
-    """Adds two numbers and returns the result."""
-    return a + b
-
-if __name__ == "__main__":
-    result = add_numbers(5, 10)
-    print(f"The result is: {result}")
